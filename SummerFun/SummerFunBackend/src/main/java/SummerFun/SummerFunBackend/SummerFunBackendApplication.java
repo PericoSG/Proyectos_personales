99d517301b41fbf68a4d9a89a9893ce421cff465
PericoSG <4803567@alu.murciaeduca.es>
2025-08-11 00:17:39 +0200

    Añadido nuevo proyecto SummerFun (Gestión de actividades de verano)

---
+++ SummerFun/SummerFunBackend/src/main/java/SummerFun/SummerFunBackend/SummerFunBackendApplication.java
@@ -0,0 +1,13 @@
+package SummerFun.SummerFunBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SummerFunBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SummerFunBackendApplication.class, args);
+	}
+
+}
---
+++ SummerFun/SummerFunBackend/src/test/java/SummerFun/SummerFunBackend/SummerFunBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package SummerFun.SummerFunBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SummerFunBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
