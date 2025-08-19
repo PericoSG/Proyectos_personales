29d3d60a49cf5f55d6360555cef47b5f02dc894a
PericoSG <4803567@alu.murciaeduca.es>
2025-08-20 01:29:34 +0200

    Empezado iniciar sesión

--- SummerFun/SummerFunFrontend/src/app/app.routes.ts
+++ SummerFun/SummerFunFrontend/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { IndexComponent } from './index/index.component';
+import { LoginComponent } from './auth/login/login.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: "inicio", pathMatch: 'full' },
-  { path: "inicio", component: IndexComponent }
+  { path: "inicio", component: IndexComponent },
+  { path: "login", component: LoginComponent }
 ];
