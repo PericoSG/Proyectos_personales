8b91ec14085a8137ca66192886cec0c4398fa6af
PericoSG <4803567@alu.murciaeduca.es>
2025-12-24 14:50:43 +0100

    Sección home hecha (falta adaptar menu)

--- benidormFest2026_soloAngular/src/app/components/home/home.component.ts
+++ benidormFest2026_soloAngular/src/app/components/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
-
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [MatCardModule, MatIconModule],
   templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
 
