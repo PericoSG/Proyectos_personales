a6f2b488150ffd2a012e3eab6ebae712518f47f7
PericoSG <4803567@alu.murciaeduca.es>
2025-12-26 11:24:21 +0100

    Array participantes configurado, falta añadir el resto + modal + estilo

--- benidormFest2026_soloAngular/src/app/components/participants/participants.component.ts
+++ benidormFest2026_soloAngular/src/app/components/participants/participants.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { MatCard, MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-participants',
-  imports: [],
+  imports: [MatCardModule],
   templateUrl: './participants.component.html',
   styleUrl: './participants.component.css'
 })
 export class ParticipantsComponent {
+
+  participants = [
+    { name: 'Asha', song: 'Turista', image: 'img/asha.jpg', description: '' },
+    { name: 'Aytat', song: 'Dopamina', image: 'img/aytat.jpg', description: '' },
+    { name: 'Dani J', song: 'Bailandote', image: 'img/danij.jpg', description: '' }
+  ];
 
 }
