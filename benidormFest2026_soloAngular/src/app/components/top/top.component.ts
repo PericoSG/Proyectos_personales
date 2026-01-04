5debf5ffb195d3a472c04cafc332421ca15c9a11
PericoSG <4803567@alu.murciaeduca.es>
2026-01-04 18:14:00 +0100

    layout hecho para hacer el top

--- benidormFest2026_soloAngular/src/app/components/top/top.component.ts
+++ benidormFest2026_soloAngular/src/app/components/top/top.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { ParticipantsService } from '../../services/participants.service';
+import { MatCard } from '@angular/material/card';
 
 @Component({
   selector: 'app-top',
-  imports: [],
+  imports: [MatCard],
   templateUrl: './top.component.html',
   styleUrl: './top.component.css'
 })
 export class TopComponent {
 
+  participants: any;
+  finalTop: any;
+
+  constructor(private participant: ParticipantsService) { }
+
+  ngOnInit() {
+    this.finalTop = Array.from({ length: 18 }, () => null);
+    console.log(this.finalTop)
+
+    return this.participants = this.participant.getParticipants();
+  }
 }
